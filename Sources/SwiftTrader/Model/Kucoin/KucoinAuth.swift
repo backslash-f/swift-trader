09e28043369cf57a84fc1aced058d8f3448e072c
Fernando Fernandes <fernando.fernandes@labtwin.com>
2022-01-31 10:21:20 +0100

    Make KucoinAuth public

--- Sources/SwiftTrader/Model/Kucoin/KucoinAuth.swift
+++ Sources/SwiftTrader/Model/Kucoin/KucoinAuth.swift
@@ -1,6 +1,6 @@
 //
-//  KucoinAuthentication.swift
-//  
+//  KucoinAuth.swift
+//
 //
 //  Created by Fernando Fernandes on 29.01.22.
 //
@@ -9,7 +9,18 @@ import Foundation
 
 /// Holds data required to authenticate the requests against Kucoin APIs.
 public struct KucoinAuth {
+
+    // MARK: - Properties
+
     public let apiKey: String
     public let apiSecret: String
     public let apiPassphrase: String
+
+    // MARK: - Lifecycle
+
+    public init(apiKey: String, apiSecret: String, apiPassphrase: String) {
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
+        self.apiPassphrase = apiPassphrase
+    }
 }
