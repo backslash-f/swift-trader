665e14b7c2d4122e7a284a0c1db15322ee2c20ac
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-03-11 19:13:52 +0100

    Conform BinanceAuth/KucoinAuth to RawRepresentable
     - Allows for using @AppStorage.

---
+++ Sources/SwiftTrader/Model/Kucoin/KucoinAuth/KucoinAuth+RawRepresentable.swift
@@ -0,0 +1,29 @@
+//
+//  KucoinAuth+RawRepresentable.swift
+//
+//
+//  Created by Fernando Fernandes on 06.03.24.
+//
+
+import Foundation
+
+extension KucoinAuth: RawRepresentable {
+    public typealias RawValue = String
+
+    public init?(rawValue: RawValue) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode(KucoinAuth.self, from: data) else {
+            return nil
+        }
+        self = result
+    }
+    
+    public var rawValue: RawValue {
+        guard let data = try? JSONEncoder().encode(self) else {
+            return "Failed to encode data"
+        }
+        guard let jsonString = String(data: data, encoding: .utf8) else {
+            return "Faile to convert data to String"
+        }
+        return jsonString
+    }}
