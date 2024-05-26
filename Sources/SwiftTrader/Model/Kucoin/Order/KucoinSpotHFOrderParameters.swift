2b88188e4b45da0f77ea3fb40f21baee0e91bbcf
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-05-26 23:44:13 +0200

    Create the KucoinSpotHFOrderParameters

---
+++ Sources/SwiftTrader/Model/Kucoin/Order/KucoinSpotHFOrderParameters.swift
@@ -0,0 +1,39 @@
+//
+//  KucoinSpotHFOrderParameters.swift
+//
+//
+//  Created by Fernando Fernandes on 23.05.24.
+//
+
+import Foundation
+
+/// Encapsulates parameters to place a spot HF order.
+///
+/// https://www.kucoin.com/docs/rest/spot-trading/spot-hf-trade-pro-account/place-hf-order
+public struct KucoinSpotHFOrderParameters {
+
+    // MARK: - Properties
+
+    public let clientOid: String
+    public let symbol: String
+    public let type: KucoinOrderType
+    public let size: String
+    public let price: String
+    public let side: OrderSide
+
+    // MARK: - Lifecycle
+
+    public init(clientOid: String = UUID().uuidString,
+                symbol: String,
+                type: KucoinOrderType,
+                size: String,
+                price: String,
+                side: OrderSide) {
+        self.clientOid = clientOid
+        self.symbol = symbol
+        self.type = type
+        self.size = size
+        self.price = price
+        self.side = side
+    }
+}
