969e498e34728a7fe42f9a9979075e23dc5491a7
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-05-29 09:22:18 +0200

    Fix/Update documentation

--- Sources/SwiftTrader/SwiftTrader+KucoinSpotHF.swift
+++ Sources/SwiftTrader/SwiftTrader+KucoinSpotHF.swift
@@ -17,38 +17,48 @@ public extension SwiftTrader {
     /// This function calculates the distance of the price of each order
     /// using the parameters of the given `SwiftTraderOrderInput` instance.
     ///
-    /// https://www.kucoin.com/docs/rest/spot-trading/spot-hf-trade-pro-account/place-multiple-orders
+    /// [Kucoin](https://www.kucoin.com/docs/rest/spot-trading/spot-hf-trade-pro-account/place-multiple-orders)
     ///
     /// - Parameter orderInput: `SwiftTraderMultiLimitOrderInput` instance that encapsulates
-    /// all the arguments required for submiting multiple orders against one asset.
+    /// all the arguments required for submitting multiple orders against one asset.
     /// - Returns: An instance of `KucoinHFPlaceMultiOrdersResponse` or `SwiftTraderError`.
     ///
-    /// - Example: As a simple example on the output of this function:
-    ///   - Consider the max bid of PEPE-USDT to be "0.00000121"
-    ///  (`SwiftTraderMultiLimitOrderInput.maxBid`)
-    ///   - The first order should be above the max bid by 0,16%
-    ///  (`SwiftTraderMultiLimitOrderInput.initialPriceIncrement`)
-    ///   - The second, third, fourth and fifth orders should increment the previous price by 0,1%
-    ///  (`SwiftTraderMultiLimitOrderInput.priceIncrement`)
-    ///   - The total funds for the 5 orders is 50
-    ///  (`SwiftTraderMultiLimitOrderInput.totalFunds`)
+    /// - Example: consider a new listed coin called "NEW"; the strategy is to create five buy orders for it as soon as
+    /// it gets listed, based on its current max bid. Via the `orderInput`, the price of the first order is set to be 11% higher,
+    /// while subsequent order prices are set to be 1% higher (compared to previous prices). In short:
+    ///   - The current max bid of NEW-USDT is "0.046"
+    ///     - (`SwiftTraderMultiLimitOrderInput.maxBid`)
+    ///   - The first order should be above the max bid by 0.11
+    ///     - (`SwiftTraderMultiLimitOrderInput.initialPriceIncrement`)
+    ///   - The second, third, fourth, and fifth orders should increment the previous price by 0.01
+    ///     - (`SwiftTraderMultiLimitOrderInput.priceIncrement`)
+    ///   - The total funds for the five orders is 50
+    ///     - (`SwiftTraderMultiLimitOrderInput.totalFunds`)
     ///
-    ///  Based on the above input, the price and size for the 5 buy/limit PEPE orders will be:
+    ///  Based on the above input, the price and size for the five buy/limit NEW-USDT orders will be:
     ///   ```
     ///   Prices:
-    ///   "0.00000140", // 0,16% of the maxBid
-    ///   "0.00000154", // 0,1% of the previous price
-    ///   "0.00000170", // 0,1% of the previous price
-    ///   "0.00000187", // 0,1% of the previous price
-    ///   "0.00000206"  // 0,1% of the previous price
+    ///   "0.051" // 0,11% of the highest bid
+    ///   "0.052" // 0,01% of the previous price
+    ///   "0.053" // 0,01% of the previous price
+    ///   "0.054" // 0,01% of the previous price
+    ///   "0.055" // 0,01% of the previous price
     ///
-    ///   Sizes:
-    ///   "7142857", (50/5)/price of first order
-    ///   "6493506", (50/5)/price of second order
-    ///   "5882352", (50/5)/price of third order
-    ///   "5347593", (50/5)/price of fourth order
-    ///   "4854368"  (50/5)/price of fifth order
+    ///   Sizes (the function rounds down the size to the nearest whole number):
+    ///   "196" // (50/5)/price of first order
+    ///   "192" // (50/5)/price of second order
+    ///   "188" // (50/5)/price of third order
+    ///   "185" // (50/5)/price of fourth order
+    ///   "181" // (50/5)/price of fifth order
     ///   ```
+    ///
+    ///   When dealing with new listings, it's crucial to try entering a position as quickly as possible,
+    ///   even if that means paying more.
+    ///
+    ///   To quickly exit the position is as important, so just after the above buying orders are submitted,
+    ///   one could trigger five selling orders targeting the desired profit percentage, for example, 60% (conservative),
+    ///   100% or 200% of the given highest bid or last order price (some new listings could go as high as 800%,
+    ///   or even more).
     func kucoinSpotHFPlaceMultipleOrders(
         _ orderInput: SwiftTraderMultiLimitOrderInput
     ) async throws -> Result<KucoinHFPlaceMultiOrdersResponse, SwiftTraderError> {
