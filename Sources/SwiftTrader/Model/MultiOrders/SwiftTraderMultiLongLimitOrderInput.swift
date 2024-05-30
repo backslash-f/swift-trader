16d75674b7fece48f2134973f5b9fbcda9628065
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-05-30 23:29:21 +0200

    Fix documentation / Lint

--- Sources/SwiftTrader/Model/MultiOrders/SwiftTraderMultiLongLimitOrderInput.swift
+++ Sources/SwiftTrader/Model/MultiOrders/SwiftTraderMultiLongLimitOrderInput.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/// Encapsulates all the arguments required for submiting multiple limit orders at once to supported exchanges.
+/// Encapsulates all the arguments required for submiting multiple long limit orders at once to supported exchanges.
 public struct SwiftTraderMultiLongLimitOrderInput {
 
     // MARK: - Properties
@@ -26,12 +26,13 @@ public struct SwiftTraderMultiLongLimitOrderInput {
     /// Kucoin exchange, buy/limit orders and five orders at once.
     ///
     /// - Parameters:
-    ///   - symbol: For example BTCUSDT
+    ///   - symbol: For example BTC-USDT
     ///   - initialPrice: The asset price from where the calculations should begin, for example "68833.24".
     ///   - initialPriceIncrement: The price of the first order, based on the `initialPrice`.
     ///   The value indicates the percentage. For example, 0,4 means that the price of the first order will
     ///   be 40% of the given `initialPrice`.
-    ///   - priceIncrement: How much the price of subsequent orders (second, third, fourth, and fifth) should go up.
+    ///   - priceIncrement: How much the price of subsequent orders (second, third, fourth, and fifth)
+    ///   should go **up**.
     ///   The value indicates the percentage. For example: 0,1 = 10%.
     ///   - totalFunds: The total funds available for the operation. The number will be distributed evenly
     ///   between all submitted orders.
