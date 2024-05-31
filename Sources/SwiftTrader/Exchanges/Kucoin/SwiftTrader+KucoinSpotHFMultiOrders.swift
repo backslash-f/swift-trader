ac15bf6ac643a75ee6764ad7a20c9071f9c0ee7d
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-05-31 10:13:22 +0200

    Use the KucoinSpotHFShortLimitOrdersResult
     - Fix/improve/add documentation.

--- Sources/SwiftTrader/Exchanges/Kucoin/SwiftTrader+KucoinSpotHFMultiOrders.swift
+++ Sources/SwiftTrader/Exchanges/Kucoin/SwiftTrader+KucoinSpotHFMultiOrders.swift
@@ -12,7 +12,7 @@ public extension SwiftTrader {
 
     // MARK: - Orders
 
-    /// Places multiple orders.
+    /// Places multiple **long** **limit** orders.
     ///
     /// This function calculates the distance of the price of each order using the parameters of the given
     /// `SwiftTraderMultiLongLimitOrderInput` instance.
@@ -20,40 +20,41 @@ public extension SwiftTrader {
     /// [Kucoin](https://www.kucoin.com/docs/rest/spot-trading/spot-hf-trade-pro-account/place-multiple-orders)
     ///
     /// - Parameter orderInput: `SwiftTraderMultiLongLimitOrderInput` instance that encapsulates
-    /// all the arguments required for submitting multiple orders (five) against one asset.
-    /// - Returns: An instance of `KucoinHFPlaceMultiOrdersResponse` or `SwiftTraderError`.
+    /// all the arguments required for submitting multiple long limit orders (five) against one asset.
+    /// - Returns: An instance of `KucoinSpotHFLongLimitOrdersResult` or `SwiftTraderError`.
+    /// The `KucoinSpotHFLongLimitOrdersResult` encapsulates all the submitted orders, including prices and sizes.
     ///
     /// - Note: Five orders are created at once; the number can't go higher, as this is a limitation of the Kucoin API.
     ///
     /// - Example: consider a new listed coin called "NEW"; the strategy is to create five buy orders for it as soon as
-    /// it gets listed, based on its current max bid. Via the `orderInput`, the price of the first order is set to be
-    /// 11% higher, while subsequent order prices are set to be 1% higher (compared to previous prices).
+    /// it gets listed, based on its current max bid. Via the `orderInput`, the price of the *first order* is set to be
+    /// 26% higher, while subsequent order prices are set to be 1% higher (compared to previous prices).
     ///
     /// In short:
     ///   - The current max bid of NEW-USDT is "0.046"
     ///     - (`SwiftTraderMultiLongLimitOrderInput.initialPrice`)
-    ///   - The first order should be above the max bid by 0.11
+    ///   - The first order should be above the max bid by 0.26
     ///     - (`SwiftTraderMultiLongLimitOrderInput.initialPriceIncrement`)
-    ///   - The second, third, fourth, and fifth orders should increment the previous price by 0.01
+    ///   - The 2ⁿᵈ, 3ʳᵈ, 4ᵗʰ, and 5ᵗʰ price orders should be equal to the previous price order incremented by 0.01
     ///     - (`SwiftTraderMultiLongLimitOrderInput.priceIncrement`)
-    ///   - The total funds for the five orders is 50
+    ///   - The total funds for the five orders is 260
     ///     - (`SwiftTraderMultiLongLimitOrderInput.totalFunds`)
     ///
     ///  Based on the above input, the price and size for the five buy/limit NEW-USDT orders will be:
     ///   ```
     ///   Prices:
-    ///   "0.051" // 0,11% of the highest bid
-    ///   "0.052" // 0,01% of the previous price
-    ///   "0.053" // 0,01% of the previous price
-    ///   "0.054" // 0,01% of the previous price
-    ///   "0.055" // 0,01% of the previous price
+    ///   "0.058" // 0.26% of the highest bid
+    ///   "0.059" // 0.01% of the previous order price
+    ///   "0.060" // 0.01% of the previous order price
+    ///   "0.061" // 0.01% of the previous order price
+    ///   "0.062" // 0.01% of the previous order price
     ///
     ///   Sizes (the function rounds down the size to the nearest whole number):
-    ///   "196" // (50/5)/price of first order
-    ///   "192" // (50/5)/price of second order
-    ///   "188" // (50/5)/price of third order
-    ///   "185" // (50/5)/price of fourth order
-    ///   "181" // (50/5)/price of fifth order
+    ///   "896" // (260/5)/price of first order
+    ///   "881" // (260/5)/price of second order
+    ///   "866" // (260/5)/price of third order
+    ///   "852" // (260/5)/price of fourth order
+    ///   "838" // (260/5)/price of fifth order
     ///   ```
     ///
     ///   When dealing with new listings, it's crucial to try entering a position as quickly as possible,
@@ -92,9 +93,53 @@ public extension SwiftTrader {
         }
     }
 
+    /// Places multiple **short** **limit** orders.
+    ///
+    /// This function calculates the distance of the price of each order using the parameters of the given
+    /// `SwiftTraderMultiShortLimitOrderInput` instance.
+    ///
+    /// [Kucoin](https://www.kucoin.com/docs/rest/spot-trading/spot-hf-trade-pro-account/place-multiple-orders)
+    ///
+    /// - Parameter orderInput: `SwiftTraderMultiShortLimitOrderInput` instance that encapsulates
+    /// all the arguments required for submitting multiple short limit orders (five) against one asset.
+    /// - Returns: An instance of `KucoinSpotHFShortLimitOrdersResult` or `SwiftTraderError`.
+    /// The `KucoinSpotHFShortLimitOrdersResult` encapsulates all the submitted orders, including prices and sizes.
+    ///
+    /// - Note: Five orders are created at once; the number can't go higher, as this is a limitation of the Kucoin API.
+    ///
+    /// - Example: consider a coin called "NEW", with a total of 4333 units acquired. The strategy involves creating
+    /// five orders, each with a size of 4333 units. Using the `orderInput`, the cost of the first order is 66% higher
+    /// than a chosen initial price, while the prices of subsequent orders are 1% higher than the previous ones.
+    ///
+    /// In short:
+    ///   - The chosen initial NEW-USDT price is "0.046"
+    ///     - (`SwiftTraderMultiShortLimitOrderInput.initialPrice`)
+    ///   - The first order should be above the max bid by 0.66
+    ///     - (`SwiftTraderMultiShortLimitOrderInput.targetProfitPercentage`)
+    ///   - The 2ⁿᵈ, 3ʳᵈ, 4ᵗʰ, and 5ᵗʰ price orders should be equal to the previous price order decremented by 0.01
+    ///     - (`SwiftTraderMultiShortLimitOrderInput.priceDecrement`)
+    ///   - The total size available is 4333
+    ///     - (`SwiftTraderMultiShortLimitOrderInput.totalSize`)
+    ///
+    ///  Based on the above input, the price and size for the five short/limit NEW-USDT orders will be:
+    ///   ```
+    ///   Prices:
+    ///   "0.076" // 0.66% of the chosen initial price
+    ///   "0.075" // 0.01% of the previous order price
+    ///   "0.074" // 0.01% of the previous order price
+    ///   "0.073" // 0.01% of the previous order price
+    ///   "0.072" // 0.01% of the previous order price
+    ///
+    ///   Order size (the function rounds down the size to the nearest whole number):
+    ///   "4333" // Each one of the five orders has the same size
+    ///   ```
+    ///
+    ///   Profits can be maximized by setting the same size for all selling orders. Once one selling order is filled
+    ///   (hopefully the one with the highest price), all available assets are sold,
+    ///   bringing the highest possible return.
     func kucoinSpotHFPlaceMultipleShortLimitOrders(
         _ orderInput: SwiftTraderMultiShortLimitOrderInput
-    ) async throws -> Result<KucoinHFPlaceMultiOrdersResponse, SwiftTraderError> {
+    ) async throws -> Result<KucoinSpotHFShortLimitOrdersResult, SwiftTraderError> {
 
         guard let auth = kucoinAuth else {
             return .failure(.kucoinMissingAuthentication)
@@ -111,7 +156,7 @@ public extension SwiftTrader {
             guard let response = model as? KucoinHFPlaceMultiOrdersResponse else {
                 return .failure(.unexpectedResponse(modelString: "\(model)"))
             }
-            return .success(response)
+            return .success(.init(orders: request.orders, response: response))
         case .failure(let error):
             let swiftTraderError = handle(
                 networkRequestError: error,
